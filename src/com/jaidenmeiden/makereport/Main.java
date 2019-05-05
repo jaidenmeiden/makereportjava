e40e6adef430e3532442f78993aecb31874e1ae3
Jaiden Escudo de roble <jailander49@hotmail.com>
2019-05-05 10:42:04 -0500

    Se construye un proyecto para generar un archivo

---
+++ src/com/jaidenmeiden/makereport/Main.java
@@ -0,0 +1,22 @@
+package com.jaidenmeiden.makereport;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Report report = new Report();
+		report.setNameFile("reporte");
+		report.setExtension("txt");
+		report.setTitle(":: REPORTE:: \n");
+		String content = report.getTitle();
+		for (int i = 0; i < 5; i++) {
+			content += "\nReporte : " + i;
+		}
+		report.setContent(content);
+		report.makeReport();
+		
+		System.out.println("Guardo el archivo!");
+
+	}
+}
